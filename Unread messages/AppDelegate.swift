81ac37eb9aa80aed9ee22024feaac18e705f4b0d
Andrew Dietrich <adietrich@zendesk.com>
2023-05-10 09:39:52 -0500

    Initial commit of unread badge count app

---
+++ Unread messages/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Unread messages
+//
+//  Created by Andrew Dietrich on 5/9/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        return true
+    }
+}
---
+++ Unread messages/Cards/ShowUnreadCountCell.swift
@@ -0,0 +1,75 @@
+//
+//  InitializeSDKCardCell.swift
+//  hello_world
+//
+//  Created by Andrew Dietrich on 3/7/23.
+//
+
+import UIKit
+
+
+class ShowUnreadCountCell: UITableViewCell {
+        
+    @IBOutlet weak var cardTitle: UILabel!
+    @IBOutlet weak var cardDescription: UILabel!
+    @IBOutlet weak var callToActionButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    var clickHandler: (() -> Void)?
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styling()
+    }
+    
+    @IBAction func callToAction(_ sender: Any) {
+        
+        clickHandler?()
+        
+    }
+    
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+      if #available(iOS 12.0, *) {
+          if traitCollection.userInterfaceStyle == .light {
+              self.contentView.layer.backgroundColor = UIColor.white.cgColor
+          } else {
+              self.contentView.layer.backgroundColor = UIColor(named: "cardBackgroundColor")?.cgColor
+              self.contentView.layer.borderColor = UIColor(named: "cardBorderColor")?.cgColor
+          }
+      }
+    }
+        
+    func styling() {
+        self.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        cardDescription.text = "You can retrieve the current total number of unread messages by calling unreadMessageCountChanged on Messaging on your Zendesk SDK instance."
+
+        self.layer.cornerRadius = 14
+        
+        self.contentView.layer.borderWidth = 1
+        self.contentView.layer.cornerRadius = 14
+        self.contentView.layer.masksToBounds = true
+        
+        let backgroundView = UIView.init(frame: contentView.frame)
+        backgroundView.backgroundColor = UIColor.clear
+        
+        if #available(iOS 12.0, *) {
+            if traitCollection.userInterfaceStyle == .light {
+                self.contentView.layer.borderColor = UIColor(named: "cardBorderColor")?.cgColor
+                self.contentView.layer.backgroundColor = UIColor.white.cgColor
+            } else {
+                self.contentView.layer.borderColor = UIColor(named: "cardBorderColor")?.cgColor
+                self.contentView.layer.backgroundColor = UIColor(named: "cardBackgroundColor")?.cgColor
+            }
+        }
+        
+        self.callToActionButton.backgroundColor = UIColor(named: "cardButtonColor")
+        self.callToActionButton.titleLabel?.tintColor = UIColor(named: "cardButtonLabelColor")
+        self.callToActionButton.layer.cornerRadius = 14
+
+        self.backgroundView = backgroundView
+    }
+    
+}
